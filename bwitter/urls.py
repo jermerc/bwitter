4b0415f78f7e00e81d4b15b1c5e61247c0fc7855
Jermaine Mercado <jmercado@uoguelph.ca>
2020-06-13 12:36:28 -0400

    Added CRUD functionality for posts.

--- bwitter/urls.py
+++ bwitter/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='bwitter-home'),
+    path('home/', PostListView.as_view(), name='bwitter-home'),
     path('about/', views.about, name='bwitter-about'),
-    path('', views.redirectHomepage)
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('', views.redirectHomepage),
+    path('post/new_post', PostCreateView.as_view(), name='post-form'),
 ]
+
--- bwitter/views.py
+++ bwitter/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 def home(request):
@@ -17,3 +19,46 @@ def about(request):
 def redirectHomepage(request):
     response = redirect('/home/')
     return response
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'bwitter/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # Sorts posts newest to oldest.
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'bwitter/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    template_name = 'bwitter/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'bwitter/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
